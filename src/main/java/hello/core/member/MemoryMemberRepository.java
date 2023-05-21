97e4e018c3123b742cc86eba9f0bfe5327e98aab
cyyoun <cyyoun41@gmail.com>
2023-05-21 11:49:48 +0900

    Ch2_회원도메인 개발과 실행

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+
+
+    private static Map<Long, Member> store = new HashMap<>(); // 동시성 문제가 있을 수 있으니 실무에서는 consistent hash 사용 권유
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
