daa9b16af52dca059c8577973f83d22e3fdfaaa7
cyyoun <cyyoun41@gmail.com>
2023-05-21 19:55:29 +0900

    Ch3_AppConfig 관심사 분리

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,8 +5,6 @@ import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository {
 
-
-
     private static Map<Long, Member> store = new HashMap<>(); // 동시성 문제가 있을 수 있으니 실무에서는 consistent hash 사용 권유
 
     @Override
