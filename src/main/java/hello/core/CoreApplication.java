69ac1e9f7999c8de329208c901394169bc7946a1
cyyoun <cyyoun41@gmail.com>
2023-05-25 19:06:16 +0900

    Ch6_탐색위치와 기본스캔대상

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //componentscan 포함되어 있음
 public class CoreApplication {
 
 	public static void main(String[] args) {
