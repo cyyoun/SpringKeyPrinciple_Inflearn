804701a0a072ecf061709e33473eedd7c1bbc056
cyyoun <cyyoun41@gmail.com>
2023-05-25 18:57:48 +0900

    Ch6_컴포넌트스캔과 의존관계 자동 주입

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -28,5 +28,14 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
 
-            }
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean.getClass() = " + bean.getClass()); //class hello.core.AppConfig$$EnhancerBySpringCGLIB
+    }
+
 }
