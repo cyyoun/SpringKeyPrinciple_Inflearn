1a82cebda6222da2593e0f622ade948d59e34ded
cyyoun <cyyoun41@gmail.com>
2023-05-25 18:26:24 +0900

    Ch5_@Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -15,12 +14,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig { // 역할과 구현 클래스를 한눈에 들어옴. APP 전체 구성이 어떻게 되어 있는지 빠르게 파악할 수 있음
 
+
+    /* 아래처럼 하면 싱글톤 패턴이 깨지지 않을까..?? */
+    // @Bean memberService() --> new MemoryMemberRepository()
+    // @Bean orderService() --> new MemoryMemberRepository()
+
     @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
-    public static MemoryMemberRepository memberRepository() {
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
     @Bean
